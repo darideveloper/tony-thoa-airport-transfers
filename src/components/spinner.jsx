30e9f42ec45f0c3b77edb96b550ec41a08451db9
daridev <darideveloper@gmail.com>
2023-07-05 13:45:10 -0600

    feat (components/vip-code): loading spinner

---
+++ src/components/spinner.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+export default function Spinner ({size="l", alt=false}) {
+
+  const className = size == "l" ? "w-40" : "w-10"
+
+  return (
+    <img 
+      src={alt ?  "./imgs/spinner_alt.gif" : "./imgs/spinner.gif"} 
+      alt="loading spinner" 
+      className={className} />
+  )
+}
+
+Spinner.propTypes = {
+  size: PropTypes.string,
+  alt: PropTypes.bool,
+}
--- src/components/vip-code.jsx
+++ src/components/vip-code.jsx
@@ -3,18 +3,27 @@ import Input from './input'
 import { useState } from 'react'
 
 import SubmitBtn from './submit-btn'
+import Spinner from './spinner'
 
 import { validateVipCode } from '../api/vip-code'
 
 export default function VipCode() {
 
   const [vipCode, setVipCode] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
+
+    // Show spinner while loading
+    setIsLoading(true)
+    console.log({ isLoading })
+
     e.preventDefault()
-    console.log({ vipCode })
 
+    // Validate vip code with api
     validateVipCode(vipCode).then(isValid => {
+
+      // Show alerts
       if (isValid) {
         Swal.fire(
           'Vip Code Validated!',
@@ -28,8 +37,10 @@ export default function VipCode() {
           'error'
         )
       }
-    })
 
+      // Hide spinner
+      setIsLoading(false)
+    })
   }
 
   return (
@@ -37,40 +48,50 @@ export default function VipCode() {
       className={`
         container w-5/6
         flex items-center justify-start
-        mb-10
+        h-28
       `}
       onSubmit={(e) => handleSubmit(e)}
     >
-      <fieldset
-        className={`
-          w-full
-          flex items-start justify-center flex-col
-          md:w-8/12 md:flex-row
-          sm:w-10/12
-          lg:w-5/12
-        `}
-      >
-        <Input
-          label='You have a VIP Code?'
-          type="text"
-          placeholder='12345'
-          name='vipCode'
-          value={vipCode}
-          handleUpdate={(e) => setVipCode(e.target.value)}
-        />
-
-        <div className={`
-            wrapper-submit
-            w-32
-            md:mt-10 md:ml-5 
-          `}>
-          <SubmitBtn 
-            value='Check'
-            extraClass={`w-32 py-2`}
+      {
+        isLoading
+          ?
+          <Spinner
+            size="s"
+            alt={true}
           />
-        </div>
-      </fieldset>
+          :
 
+          <fieldset
+            className={`
+              w-full
+              flex items-start justify-center flex-col
+              md:w-8/12 md:flex-row
+              sm:w-10/12
+              lg:w-5/12
+            `}
+          >
+            <Input
+              label='You have a VIP Code?'
+              type="text"
+              placeholder='12345'
+              name='vipCode'
+              value={vipCode}
+              handleUpdate={(e) => setVipCode(e.target.value)}
+            />
+
+            <div className={`
+              wrapper-submit
+              w-32
+              md:mt-10 md:ml-5 
+            `}>
+              <SubmitBtn
+                value='Check'
+                extraClass={`w-32 py-2`}
+              />
+            </div>
+          </fieldset>
+
+      }
     </form>
   )
 }
